b54c086102f0e946d2acb4ea55362cea0b10d821
Andrew Lee <leeas@stolaf.edu>
2016-11-29 21:49:01 -0600

    test

--- source/test.py
+++ source/test.py
@@ -1,9 +1,5 @@
-# from os import *
+import IPython
 import subprocess
+IPython.embed()
 bashCommand = "gprolog"
 output = subprocess.check_output(['bash','-c', bashCommand])
-
-# userinput = raw_input()
-# userinput = userinput[0:len(userinput)-1]
-# print(userinput)
-# os.system(gprolog)
