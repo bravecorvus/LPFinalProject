f2be316b3571f6e0de137d5a656b14130bb83bfe
Andrew Lee <leeas@stolaf.edu>
2016-11-29 21:24:57 -0600

    eh

--- source/main.py
+++ source/main.py
@@ -1,5 +1,17 @@
 from pocketsphinx import LiveSpeech
 from pyswip import Prolog
+prolog = Prolog()
+userinput = ""
+print('Welcome to SWI-Prolog (Multi-threaded, 64 bits, Version 7.2.3)\nCopyright (c) 1990-2015 University of Amsterdam, VU Amsterdam\nSWI-Prolog comes with ABSOLUTELY NO WARRANTY. This is free software,\nand you are welcome to redistribute it under certain conditions.\nPlease visit http://www.swi-prolog.org for details")\n\n For help, use ?- help(Topic). or ?- apropos(Word).')
+prolog.consult("consult("dictionary.pl")")
+while True:
+    userinput = input("?- ")
+    if userinput == "exit()":
+        break
+    else:
+        if userinput[len(userinput)] == ".":
+            userinput = userinput[0:len(userinput)-1]
+        prolog.consult(userinput)
 
 # Primary Action File. Does all the work between listening for human input, and passing along the text to Prolog
 # Once Prolog returns the appropriate function, Python will execute it and return the state back to suspend until the keyword is invoked again
