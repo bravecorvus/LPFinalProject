478e96c0b8117715660ea64dc0d17df29bfcc24a
Andrew Lee <leeas@stolaf.edu>
2016-11-29 21:45:18 -0600

    test

--- source/test.py
+++ source/test.py
@@ -1,6 +1,9 @@
-from os import *
+# from os import *
+import subprocess
+bashCommand = "gprolog"
+output = subprocess.check_output(['bash','-c', bashCommand])
 
-userinput = raw_input()
-userinput = userinput[0:len(userinput)-1]
-print(userinput)
-os.system(gprolog)
+# userinput = raw_input()
+# userinput = userinput[0:len(userinput)-1]
+# print(userinput)
+# os.system(gprolog)
