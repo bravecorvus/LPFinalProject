582b1ca9a6a0fb2fd259fbc31074332b3e75265e
Andrew Lee <leeas@stolaf.edu>
2016-11-29 21:26:43 -0600

    ehh

--- source/main.py
+++ source/main.py
@@ -1,9 +1,3 @@
-from pocketsphinx import LiveSpeech
-from pyswip import Prolog
-prolog = Prolog()
-userinput = ""
-print('Welcome to SWI-Prolog (Multi-threaded, 64 bits, Version 7.2.3)\nCopyright (c) 1990-2015 University of Amsterdam, VU Amsterdam\nSWI-Prolog comes with ABSOLUTELY NO WARRANTY. This is free software,\nand you are welcome to redistribute it under certain conditions.\nPlease visit http://www.swi-prolog.org for details")\n\n For help, use ?- help(Topic). or ?- apropos(Word).')
-prolog.consult("consult("dictionary.pl")")
 from pyswip import Prolog, Functor, Variable, Query
 prolog = Prolog()
 userinput = ""
