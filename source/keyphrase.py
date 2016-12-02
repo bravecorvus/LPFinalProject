29f1ff2104bb6781b7d3f999a52921d9dd541bb4
Andrew Lee <leeas@stolaf.edu>
2016-12-01 21:15:54 -0600

    fix

--- source/keyphrase.py
+++ source/keyphrase.py
@@ -1,26 +1,19 @@
+from pocketsphinx import LiveSpeech
 import speech_recognition as sr
 
 # obtain audio from the microphone
-r1 = sr.Recognizer()
-r2 = sr.Recognizer()
 while True:
-	with sr.Microphone() as source:
-	    print("I am Your Assitant, say something for me to assist you")
-	    keyphrase = r1.listen(source)
-	try:
-		if r1.recognize_sphinx(keyphrase) == "hey assistant":
-			print("Listening for commands")
-			with sr.Microphone() as source:
-			    command = r2.listen(source)
-			try:
-	            print("Assistant registered the phrase " + r2.recognize_sphinx(command))
-	        except sr.UnknownValueError:
-	            print("Assistant could not understand audio")
-	        except sr.RequestError as m:
-	            print("Sphinx error; {0}".format(m))                
-
-	except sr.UnknownValueError:
-	    print("Assistant could not understand audio")
-	except sr.RequestError as e:
-	    # print("Sphinx error; {0}".format(e))
-	    print("\n")
+	speech = LiveSpeech(lm=False, keyphrase='hey assistant', kws_threshold=1e+20)
+	for phrase in speech:
+	    print(phrase.segments(detailed=True))
+	    r = sr.Recognizer()
+		with sr.Microphone() as source:
+		    print("I am Your Assitant, say something for me to assist you")
+		    keyphrase = r.listen(source)
+		try:
+			print("Assistant registered the phrase " + r.recognize_sphinx(audio))
+		except sr.UnknownValueError:
+		    print("Assistant could not understand audio")
+		except sr.RequestError as e:
+		    # print("Sphinx error; {0}".format(e))
+		    print("\n")
