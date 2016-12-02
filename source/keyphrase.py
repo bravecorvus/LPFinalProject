050c740f50b70a1100594fae11b9b432bd5fcfe7
Andrew Lee <leeas@stolaf.edu>
2016-12-01 21:11:53 -0600

    few fixes

--- source/keyphrase.py
+++ source/keyphrase.py
@@ -1,24 +1,26 @@
 import speech_recognition as sr
 
 # obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("I am Your Assitant, say something for me to assist you")
-    keyphrase = r.listen(source)
+r1 = sr.Recognizer()
+r2 = sr.Recognizer()
+while True:
+	with sr.Microphone() as source:
+	    print("I am Your Assitant, say something for me to assist you")
+	    keyphrase = r1.listen(source)
+	try:
+		if r1.recognize_sphinx(keyphrase) == "hey assistant":
+			print("Listening for commands")
+			with sr.Microphone() as source:
+			    command = r2.listen(source)
+			try:
+	            print("Assistant registered the phrase " + r2.recognize_sphinx(command))
+	        except sr.UnknownValueError:
+	            print("Assistant could not understand audio")
+	        except sr.RequestError as m:
+	            print("Sphinx error; {0}".format(m))                
 
-# recognize speech using Sphinx
-try:
-    if r.recognize_sphinx(keyphrase) == "hey assistant":
-        try:
-            command = r.listen(source)
-            print(print("Assistant registered the phrase " + r.recognize_sphinx(command)))
-        except sr.UnknownValueError:
-            print("Assistant could not understand audio")
-        except sr.RequestError as e:
-            print("Sphinx error; {0}".format(e))                
-
-except sr.UnknownValueError:
-    print("Assistant could not understand audio")
-except sr.RequestError as e:
-    # print("Sphinx error; {0}".format(e))
-    print("\n")
+	except sr.UnknownValueError:
+	    print("Assistant could not understand audio")
+	except sr.RequestError as e:
+	    # print("Sphinx error; {0}".format(e))
+	    print("\n")
