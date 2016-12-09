795d060f440645cb1191495e50307b9c7cce483d
Andrew Lee <leeas@stolaf.edu>
2016-12-09 00:24:37 -0600

    fixes
    -removed archive/andrew, put all presentation files into parent archive directory
    -merged sender.py code into main.py. moved sender.py to arcchive/presentation2

--- source/main.py
+++ source/main.py
@@ -1,5 +1,13 @@
+#!/usr/bin/env python
+import socket
 from pocketsphinx import LiveSpeech
 import speech_recognition as sr
+
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 1025
+BUFFER_SIZE = 1024
+MESSAGE = "append1([a,b], c, X)."
 
 def actions():
     m = sr.Recognizer()
@@ -13,8 +21,14 @@ def actions():
         for count, data in enumerate(userinput):
             if count < len(userinput)-1:
                 userinput[count] = data + ', '
-        print('Will send the following term to prolog\n')
-        print("inputparser([" + ''.join(userinput) + "], X).")
+        print('\n\n\nSUCCESS\n\n\nWill send the following term to prolog\n')
+        sendstream = "inputparser([" + ''.join(userinput) + "], X)."
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((TCP_IP, TCP_PORT))
+        s.send(sendstream)
+        data = s.recv(BUFFER_SIZE)
+        s.close()
+        print("received data:", data)
     except sr.UnknownValueError:
         print("Assistant could not understand audio")
     except sr.RequestError as e:
@@ -25,7 +39,7 @@ while True:
             audio = r.listen(source)
     try:
         if r.recognize_google(audio) == 'hey assistant' or r.recognize_google(audio) == 'his system' or r.recognize_google(audio) == 'hey system' or r.recognize_google(audio) == 'his assistant' or r.recognize_google(audio) == 'assistance' or r.recognize_google(audio) == 'assistant':
-            print("This is assistant. How can I be of service?")
+            print("\n\n\nSUCCESS! \n\n\nThis is assistant. How can I be of service?")
             actions()
         # for testing purposes, we're just using the default API key
         # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY", show_all=True)`
