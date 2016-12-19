f8065850afcded202d30e0e08054ffcd5cfbde47
Magnus Cardell <cardell.magnus@gmail.com>
2016-12-18 20:17:25 -0600

    mrege

--- source/sender.py
+++ source/sender.py
@@ -4,9 +4,9 @@ import socket
 
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 1026
+TCP_PORT = 1025
 BUFFER_SIZE = 1024
-MESSAGE = "how are you"
+MESSAGE = "good thank you"
 
 print("sending data:", MESSAGE)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
