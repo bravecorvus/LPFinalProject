b6fc6e4265723cbf18cde10e72ec0c2ba1f2ba13
Andrew Lee <leeas@stolaf.edu>
2016-12-19 21:25:17 -0600

    update

---
+++ source/test2.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+import subprocess
+import httplib2
+import os
+from apiclient import discovery
+# from apiclient import service
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+import datetime
+import socket
+import speech_recognition as sr
+
+event = {
+  'summary': 'Google I/O 2015',
+  'location': '800 Howard St., San Francisco, CA 94103',
+  'description': 'A chance to hear more about Google\'s developer products.',
+  'start': {
+    'dateTime': '2015-05-28T09:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+  'end': {
+    'dateTime': '2015-05-28T17:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+  'recurrence': [
+    'RRULE:FREQ=DAILY;COUNT=2'
+  ],
+  'attendees': [
+    {'email': 'lpage@example.com'},
+    {'email': 'sbrin@example.com'},
+  ],
+  'reminders': {
+    'useDefault': False,
+    'overrides': [
+      {'method': 'email', 'minutes': 24 * 60},
+      {'method': 'popup', 'minutes': 10},
+    ],
+  },
+}
+
+event = service.events().insert(calendarId='primary', body=event).execute()
+print 'Event created: %s' % (event.get('htmlLink'))
